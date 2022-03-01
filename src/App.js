ede057c46ea72923fab950791c0b1720144915f0
williamsilveiraf <williamsilveiraf@hotmail.com>
2022-03-01 18:59:52 -0300

    feat(inputArea): add inputs specs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,75 @@
 import React from "react";
-import { Main } from "./styles";
+import { Main, Container, Logo, ContentBody, Home, Title, Description, InputArea, CTA } from "./styles";
+import { AppBar, Toolbar, TextField, InputAdornment, InputLabel, OutlinedInput, FormControl } from '@mui/material'
 
 function App() {
   return (
     <Main>
-      <h1>Initial Stage</h1>
+      <Container>
+        <AppBar position="static">
+          <Toolbar>
+            <Logo>Car Loan</Logo>
+          </Toolbar>
+        </AppBar>
+        <ContentBody>
+          <Home>
+            <Title>
+              Car loan calculator
+            </Title>
+            <Description>
+              Make a simulation and see the value of the installment of your next car
+            </Description>
+          </Home>
+          <InputArea>
+            <CTA>QuickStart</CTA>
+            <FormControl fullWidth >
+              <InputLabel htmlFor="outlined-adornment-carprice">Car Price</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-carprice"
+                startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                label="Car Price"
+                type="number"
+              />
+            </FormControl>
+            <FormControl fullWidth >
+              <InputLabel htmlFor="outlined-adornment-downpayment">Down Payment (optional)</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-downpayment"
+                startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                label="Down Payment (optional)"
+                type="number"
+              />
+            </FormControl>
+            <FormControl fullWidth >
+              <InputLabel htmlFor="outlined-adornment-tradeinvalue">Trade-in value (optional)</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-outlined-adornment-tradeinvalue"
+                startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                label="Trade-in value (optional)"
+                type="number"
+              />
+            </FormControl>
+            <FormControl fullWidth >
+              <InputLabel htmlFor="outlined-adornment-lengthofloan">Length of loan</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-outlined-adornment-lengthofloan"
+                endAdornment={<InputAdornment position="end">months</InputAdornment>}
+                label="Length of loan"
+                type="number"
+              />
+            </FormControl>
+            <FormControl fullWidth >
+              <InputLabel htmlFor="outlined-adornment-rate">Annual Percentage Rate</InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-outlined-adornment-rate"
+                endAdornment={<InputAdornment position="end">%</InputAdornment>}
+                label="Rate"
+                type="number"
+              />
+            </FormControl>
+          </InputArea>
+        </ContentBody>
+      </Container>
     </Main>
   );
 }
--- src/styles.js
+++ src/styles.js
@@ -1,3 +1,4 @@
+import { Typography } from "@mui/material";
 import styled from "styled-components";
 
 export const Main = styled.main`
@@ -6,7 +7,56 @@ export const Main = styled.main`
     bottom: 0;
     left: 0;
     right: 0;
-    background: black;
-    color: white;
     display: flex;
+`
+
+export const Container = styled.div`
+    margin: auto;
+    width: 100%;
+    height: 100%;
+    display: grid;
+    grid-template-rows: 60px 660px;
+`
+
+export const Logo = styled.h3`
+    font-family: 'Roboto';
+    font-weight: 700;
+`
+export const ContentBody = styled.section`
+    display: grid;
+    grid-template-rows: 200px 440px;
+    border: 1px solid hotpink;
+    width: 100%;
+`
+export const Home = styled.section`
+    border: 1px solid hotpink;
+    padding: 10px;
+`
+
+export const InputArea = styled.section`
+    border: 1px solid hotpink;
+    padding: 10px;
+    background-color: #F2F2F2;
+    & > * > * {
+        background-color: white;
+        margin-top: 20px;
+    }
+`
+
+export const Title = styled.h6`
+    font-weight: 700;
+    font-size: 1.3rem;
+    margin-top: 60px;
+`
+
+export const Description = styled.p`
+    font-family: 'Inter';
+    font-size: 1.2rem;
+    margin-top: 20px;
+`
+
+export const CTA = styled.h6`
+    font-weight: 700;
+    font-size: 1.3rem;
+    margin-top: 8px;
 `
