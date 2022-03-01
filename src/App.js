51769a6355469bca74a099a519c0a22e67f9277d
williamsilveiraf <williamsilveiraf@hotmail.com>
2022-03-01 01:48:59 -0300

    feat(app): setup scope, mui and styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Main } from "./styles";
 
 function App() {
   return (
-    <div className="App">
+    <Main>
       <h1>Initial Stage</h1>
-    </div>
+    </Main>
   );
 }
 
