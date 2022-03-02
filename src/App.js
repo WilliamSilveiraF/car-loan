4b16dee7d3461f8a81540b3ace9137d54ae9cfcc
williamsilveiraf <williamsilveiraf@hotmail.com>
2022-03-01 22:20:13 -0300

    feat(front): breakdown and footer done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Main, Container, Logo, ContentBody, Home, Title, Description, InputArea, CTA } from "./styles";
-import { AppBar, Toolbar, TextField, InputAdornment, InputLabel, OutlinedInput, FormControl } from '@mui/material'
+import { Main, Container, Logo, ContentBody, Home, Title, Description, InputArea, CTA, Breakdown, Footer } from "./styles";
+import { AppBar, Toolbar, Button, InputAdornment, InputLabel, OutlinedInput, FormControl } from '@mui/material'
 
 function App() {
   return (
@@ -22,7 +22,13 @@ function App() {
           </Home>
           <InputArea>
             <CTA>QuickStart</CTA>
-            <FormControl fullWidth >
+            <FormControl 
+              fullWidth 
+              sx={{
+                bgcolor: 'white',
+                mt: '20px'
+              }}
+            >
               <InputLabel htmlFor="outlined-adornment-carprice">Car Price</InputLabel>
               <OutlinedInput
                 id="outlined-adornment-carprice"
@@ -31,7 +37,13 @@ function App() {
                 type="number"
               />
             </FormControl>
-            <FormControl fullWidth >
+            <FormControl 
+              fullWidth 
+              sx={{
+                bgcolor: 'white',
+                mt: '20px'
+              }}
+            >
               <InputLabel htmlFor="outlined-adornment-downpayment">Down Payment (optional)</InputLabel>
               <OutlinedInput
                 id="outlined-adornment-downpayment"
@@ -40,7 +52,13 @@ function App() {
                 type="number"
               />
             </FormControl>
-            <FormControl fullWidth >
+            <FormControl 
+              fullWidth
+              sx={{
+                bgcolor: 'white',
+                mt: '20px'
+              }}
+            >
               <InputLabel htmlFor="outlined-adornment-tradeinvalue">Trade-in value (optional)</InputLabel>
               <OutlinedInput
                 id="outlined-adornment-outlined-adornment-tradeinvalue"
@@ -49,7 +67,13 @@ function App() {
                 type="number"
               />
             </FormControl>
-            <FormControl fullWidth >
+            <FormControl 
+              fullWidth
+              sx={{
+                bgcolor: 'white',
+                mt: '20px'
+              }}
+            >
               <InputLabel htmlFor="outlined-adornment-lengthofloan">Length of loan</InputLabel>
               <OutlinedInput
                 id="outlined-adornment-outlined-adornment-lengthofloan"
@@ -58,17 +82,49 @@ function App() {
                 type="number"
               />
             </FormControl>
-            <FormControl fullWidth >
+            <FormControl 
+              fullWidth
+              sx={{
+                bgcolor: 'white',
+                mt: '20px'
+              }}
+            >
               <InputLabel htmlFor="outlined-adornment-rate">Annual Percentage Rate</InputLabel>
               <OutlinedInput
                 id="outlined-adornment-outlined-adornment-rate"
                 endAdornment={<InputAdornment position="end">%</InputAdornment>}
-                label="Rate"
+                label="Annual Percentage Rate"
                 type="number"
               />
             </FormControl>
+            <Button 
+              variant="contained"
+              fullWidth
+              sx={{
+                mt: '20px'
+              }}
+            >
+              Calculate
+            </Button>
           </InputArea>
+          <Breakdown>
+            <CTA>Your estimated payment</CTA>
+            <h5>${ '0' }/mo. </h5>
+            <h4>Breakdown</h4>
+            <p>Car Price <span>${'0'}</span></p>
+            <p>Down Payment <span>-${'0'}</span></p>
+            <p>Trade-in value <span>-${'0'}</span></p>
+            <div></div>
+            <h3>Total loan amount<span>${'0'}</span></h3>
+            <h3>Total interest paid<span>${'0'}</span></h3>
+            <h3>Total loan & interest payment<span>${'0'}</span></h3>
+            <h3>Monthly payment<span>${'0'}</span></h3>
+          </Breakdown>
         </ContentBody>
+        <Footer>
+            <h3>Car Loan</h3>
+            <p>© Car Loan. All rights reserved.</p>
+          </Footer>
       </Container>
     </Main>
   );
--- src/styles.js
+++ src/styles.js
@@ -1,4 +1,3 @@
-import { Typography } from "@mui/material";
 import styled from "styled-components";
 
 export const Main = styled.main`
@@ -7,46 +6,45 @@ export const Main = styled.main`
     bottom: 0;
     left: 0;
     right: 0;
-    display: flex;
+    
 `
 
 export const Container = styled.div`
-    margin: auto;
-    width: 100%;
     height: 100%;
     display: grid;
-    grid-template-rows: 60px 660px;
+    & > section {
+        margin: auto;
+    }
+    grid-template-rows: 60px 1124px 60px;
+
 `
 
 export const Logo = styled.h3`
-    font-family: 'Roboto';
     font-weight: 700;
 `
 export const ContentBody = styled.section`
     display: grid;
-    grid-template-rows: 200px 440px;
-    border: 1px solid hotpink;
-    width: 100%;
+    grid-template-rows: 188px 500px 496px;
+    justify-content:center;
+    
+    @media(min-width: 768px) {
+        width: 768px;
+    }
 `
 export const Home = styled.section`
-    border: 1px solid hotpink;
     padding: 10px;
 `
 
-export const InputArea = styled.section`
-    border: 1px solid hotpink;
+export const InputArea = styled.form`
     padding: 10px;
     background-color: #F2F2F2;
-    & > * > * {
-        background-color: white;
-        margin-top: 20px;
-    }
+    
 `
 
 export const Title = styled.h6`
     font-weight: 700;
     font-size: 1.3rem;
-    margin-top: 60px;
+    margin-top: 52px;
 `
 
 export const Description = styled.p`
@@ -59,4 +57,56 @@ export const CTA = styled.h6`
     font-weight: 700;
     font-size: 1.3rem;
     margin-top: 8px;
+`
+
+export const Breakdown = styled.section`
+    padding: 10px;
+    & > h6 {
+        text-align: center;
+    }
+    & > h5 {
+        margin-top: 16px;
+        font-size: 2.25rem;
+        text-align: center;
+    }
+    & > h4 {
+        margin-top: 32px;
+        font-size: 1.75rem;
+    }
+    & > p {
+        font-size: 1.2rem;
+        color: '#F2F2F2';
+        margin-top: 16px;
+        display: flex;
+        justify-content: space-between;
+    }
+    & > p > * {
+        font-size: 1.2rem;
+    }
+    & > div {
+        width: 100%;
+        height: 2px;
+        background-color: #F2F2F2;
+        margin: 16px 0 16px 0;
+    }
+    & > h3 {
+        font-size: 1.2rem;
+        margin-top: 16px;
+        display: flex;
+        justify-content: space-between;
+    }
+`
+export const Footer = styled.footer`
+    background-color: #1876D2;
+    text-align: center;
+    color: white;
+    & > h3 {
+        margin-top: 12px;
+        font-weight: 700;
+        font-size: 1.25rem;
+        
+    }
+    & > p {
+        font-size: 0.75rem;
+    }
 `
